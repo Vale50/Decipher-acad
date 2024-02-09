68bacc8253e93eb048243fc3299c5f0109db9576
Vale50 <89451862+Vale50@users.noreply.github.com>
2024-02-09 23:19:07 +0000

    save

--- miracle/ONYINYECHI/juniorwaec2018.js
+++ miracle/ONYINYECHI/juniorwaec2018.js
@@ -82,60 +82,184 @@ let questions = [
   },
   {
     question 9: " Solve the simultaneous equations: x - 2y + 3 = 0, 3x + 2y = 7",
-    choice1: "are studying",
-    choice2: "studying",
-    choice3: "were studying",
-    choice4: "will study",
-    answer: 1
+    choice1: "x = -4, y = 1",
+    choice2: "x = -3, y = 0",
+    choice3: "x = 1, y = -3 ",
+    choice4: "x = 1, y = 2",
+    choice5: " x = 2, y = 1",
+    answer: 
   },
   {
-    question: " My aunt ___________ to Albertville tomorrow",
-    choice1: "is moving",
-    choice2: "shall moving",
-    choice3: "will be moving",
-    choice4: "was moving",
-    answer: 3
+    question 10: " The sum of 2 times a number and 3 times another number is 4. The first number minus the second number is -3. What are the numbers?",
+    choice1: "-3 and 6",
+    choice2: "-1 and 2",
+    choice3: "2 and 5",
+    choice4: "6 and 3",
+    choice5: "7 and 11",
+    answer: 
   },
   {
-    question: " Mr Kelley ____________ to church every Sunday.?",
+    question 11: "The product of 5 and the sum of 12 and a certain number is 10. What is the number?",
+    choice1: "-15",
+    choice2: "-10",
+    choice3: "-5",
+    choice4: "5",
+    choice5: "10"
+    answer: 
+  },
+  {
+    question 12: "Solve for z if <sup>4</sup>&frasl;<sub>z</sub>looks like 4/z + <sup>4</sup>&frasl;<sub>3z</sub>looks like 4/3z = <sup>z</sup>&frasl;<sub>3</sub>looks like z/3?",
+    choice1: "±3",
+    choice2: "±4",
+    choice3: "±6",
+    choice4: "±16",
+    choice5: "±24"
+    answer: 
+  },
+  {
+    question 13: " If y -2x = 3, x + y = 3. Find the values of x and y?",
+    choice1:" x = 0, y = 3",
+    choice2: "x = 3, y = 0",
+    choice3: "x = -3, y = 0",
+    choice4: " x = 0, y = 0",
+    choice5: "x = 3, y = -3",
+    answer: 
+  },
+  {
+    question 14: "If 4x + 2y = 0, 3x – y = 5, find x + y.",
+    choice1: "3",
+    choice2: "2",
+    choice3: "1",
+    choice4: "-1",
+    choice5: "-2",
+    answer: 
+  },
+  {
+    question 15: " Given that 3x + 2y = 5, 2x + 5y = 3, find <sup>x</sup>&frasl;<sub>y</sub>looks like x/y",
+    choice1:"29 ",
+    choice2: "19",
+    choice3: "17",
+    choice4: "-19",
+    choice5: "-29",
+    answer: 
+  }, 
+  {
+    question 16: "Simplify 10x - 7y - 6x + 5y ",
+    choice1: "2(4x - 2y)",
+    choice2: "2(2x - y)",
+    choice3: " 2x - y ",
+    choice4: " 2x(2x - 2y)",
+    choice5: "2(10x - 7y)",
+    answer: 
+  }, 
+  {
+    question 17: " Find the length of the diagonal of a rectangle whose length and breadth are 4m and 3m respectively",
+    choice1: "6",
+    choice2: "5",
+    choice3: "4",
+    choice4: "3",
+    choice5: "2",
+    answer: 
+  }, 
+  {
+    question 18: "Solve the equation <sup>2x-3</sup>&frasl;<sub>7</sub>looks like 2x-3/7",
+    choice1: "18",
+    choice2: "19",
+    choice3: "20",
+    choice4: "21",
+    choice5: "22",
+    answer: 
+  }, 
+  {
+    question 19: "If one-tenth of a number is added to 2, the result is half of that number, find the number. ",
+    choice1: "2.5",
+    choice2: "3.3",
+    choice3: "4.2",
+    choice4: "5.0",
+    choice5: "6.7",
+    answer: 
+  },
+  {
+    question 20: "Represent x ≤4 on the number line",
+    choice1:" ",
+    choice2: "",
+    choice3: "",
+    choice4: "",
+    choice5: "",
+    answer: 
+  }, 
+  {
+    question 21: " If a certain number K is added to 6, the result is greater than 3. Find the range of the values of K",
+    choice1: "K > 3",
+    choice2: "K > -3",
+    choice3: "K < 3",
+    choice4: "K < -3",
+    choice5: "K ≥ -3",
+    answer: 
+  },  
+  {
+    question: " ",
     choice1: "",
     choice2: "",
     choice3: "",
     choice4: "",
-    answer: 1
-  },
+    choice5: "",
+    answer: 
+  }, 
   {
-    question: " Mr Kelley ____________ to church every Sunday.?",
+    question: " ",
     choice1: "",
     choice2: "",
     choice3: "",
     choice4: "",
-    answer: 4
+    choice5: "",
+    answer: 
   },
   {
-    question: " Jason and Roland were riding their bikes when it started to rain?",
-    choice1:"present progressive",
-    choice2: "past progressive",
-    choice3: "future progressive",
-    choice4: "none of the above",
-    answer: 2
-  },
+    question: " ",
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
+    choice5: "",
+    answer: 
+  }, 
   {
-    question: " The scientist will be talking about his research at the next meeting",
-    choice1: "future progressive",
-    choice2: "past progressive",
-    choice3: "present progressive",
-    choice4: "all of the above",
-    answer: 1
-  },
+    question: " ",
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
+    choice5: "",
+    answer: 
+  }, 
   {
-    question: " I _____ to the park tomorrow with my friends.",
-    choice1:"was going ",
-    choice2: "is going",
-    choice3: "are going",
-    choice4: "will be going",
-    answer: 4
-  },
+    question: " ",
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
+    choice5: "",
+    answer: 
+  }, 
+  {
+    question: " ",
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
+    choice5: "",
+    answer: 
+  },  
+  {
+    question: " ",
+    choice1: "",
+    choice2: "",
+    choice3: "",
+    choice4: "",
+    choice5: "",
+    answer: 
+  },                            
 ];
 
 //CONSTANTS
